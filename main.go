75374da8f126b60c25202e20ee6e0c5589c47393
Lucas Bearden <lpbearden@gmail.com>
2018-09-03 22:47:32 -0400

    updated parsing of date

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func main() {
 		// if matchDate != "Results for August 31st 2018" {
 		// 	return
 		// }
-		parsedDate := parseDate(matchDate)
+		parsedDate := parseDate(s.Split(matchDate, " "))
 
 		fmt.Println()
 		fmt.Println(parsedDate[3] + " " + parsedDate[0] + ", " + parsedDate[2])
@@ -75,22 +75,21 @@ func main() {
 	c.Visit("https://www.hltv.org/results?stars=1")
 }
 
-func parseDate(input string) []string {
+func parseDate(input []string) []string {
 	// date format of [dd, mm, yyyy, monthName]
-	newInput := s.Split(input, " ")
 	date := make([]string, 4)
 
 	r, _ := regexp.Compile("[0-9]")
-	day := r.FindAllString(newInput[3], -1)
+	day := r.FindAllString(input[3], -1)
 	if len(day) == 2 {
 		date[0] = day[0] + day[1]
 	} else {
 		date[0] = "0" + day[0]
 	}
 
-	date[1] = monthMap[newInput[2]]
-	date[2] = newInput[len(newInput)-1]
-	date[3] = newInput[2]
+	date[1] = monthMap[input[2]]
+	date[2] = input[len(input)-1]
+	date[3] = input[2]
 
 	return date
 }
